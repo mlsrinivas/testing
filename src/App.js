5348d6eb4d67b6fcbca5ce327962d2bdde37fe1a
mlsrinivas <mlsrinivas70@gmail.com>
2021-09-29 14:50:50 +0530

    test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div style={{alignItems:'center', justifyContent:'center', display: 'flex', flex: 1, width: '100%', height: '100vh'}}>
+      <h1>Welcome</h1>
       <h1 style={{color:'green'}}>Hello, this is my first live application</h1>
     </div>
   );
