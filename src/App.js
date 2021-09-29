3b88ddbbcba5794eea6ebab61dec89a8e9239510
MLSrinivas70 <srinivas@banyanspace.com>
2021-09-29 12:46:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{alignItems:'center', justifyContent:'center', display: 'flex', flex: 1, width: '100%', height: '100vh'}}>
+      <h1 style={{color:'green'}}>Hello, this is my first live application</h1>
+    </div>
+  );
+}
+
+export default App;
